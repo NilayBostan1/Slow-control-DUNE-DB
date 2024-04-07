6ecc61c2be38e508e5adf099b3cc7a07ffcbc040
Nilay Bostan <83247740+nilaybostan@users.noreply.github.com>
2024-04-07 11:48:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ log_dict = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
 input_data = 'data'
 list_data_date = ['April3', 'April4']
 HV_file_names = [f'{input_data}/NP04_DCS_01_Heinz_V_{d}.csv' for d in list_data_date]
-output_folder = 'data/output'
+output_folder = 'data'
 
 
 def configure_from_args(args):
